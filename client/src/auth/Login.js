5cb5cf193b814435cb66c2aa42510ce99e7c8ec6
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-05-03 18:44:19 -0600

    first commit

---
+++ client/src/auth/Login.js
@@ -0,0 +1,9 @@
+const Login=()=>(
+   
+            <div className="container-fluid h1 p-5 text-center">
+                Login page
+            </div>
+        
+)
+
+export default Login;
---
+++ client/src/booking/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home=()=>(
+    
+            <div className="container-fluid h1 p-5 text-center">
+               <h1>Home Page</h1>
+            </div>
+        
+);
+
+export default Home;
